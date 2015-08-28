09b784dbc24b77396adbb660a2eeb5b340ffaf15
rdhamane <rohit.dhamane@gmail.com>
2015-08-28 17:17:15 +0100

    Update RunScriptsByCalling.py

--- RunScriptsByCalling.py
+++ RunScriptsByCalling.py
@@ -3,9 +3,10 @@ import os
 #Run CF_NM_UserBased-4.py with arguments
 # argv[0] = CF_NM_UserBased-4.py
 #argv[1] = Value of N - used for changing file name for trainingSet (e.g. u'+N+'.csv = u1.csv where N=1) and TestSet (e.g. ./MovieLensData/u'+N+'-test.csv' = ./MovieLensData/u1-test.csv' where N=1)
-#argv[] = Similarity file (./SValuesUserBased/'+SimilarityFile where SimilarityFile=Pearson_Sij_UserBased-test4.csv)
-#argv[] = Estimated Reating File to record RMSE values for each test.
-
+#argv[2] = Similarity file (./SValuesUserBased/'+SimilarityFile where SimilarityFile=Pearson_Sij_UserBased-test4.csv)
+#argv[3] = Estimated Reating File to record RMSE values for each test.
+#argv[4] = "Type" of similarity measure (e.g. pearson, Cosine, Adj. Cosine etc). Used at the very end while storing final RMSE values in Estimated Ratings File. It should be changed in the future.
+ 
 os.system('python CF_NM_UserBased-4.py "4" "Pearson_Sij_UserBased-test4.csv" "Estimated-Ratings-Pearson-u4-test.csv" "Pearson"')
 
 os.system('python CF_NM_UserBased-4.py "5" "Pearson_Sij_UserBased-test5.csv" "Estimated-Ratings-Pearson-u5-test.csv" "Pearson"')
